6a2c8e6aa94737ac325247c99484f20014c6bfc3
Jeongseop Lim <jseoplim@gmail.com>
2024-10-08 01:39:52 +0900

    feat: Define MVN class

--- em/__init__.py
+++ em/__init__.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable, Iterator
+from dataclasses import dataclass
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, TypeAlias
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
@@ -58,3 +59,33 @@ def generate_dataset_parts(
         )
         for i in range(k)
     )
+
+
+# cluster 수 z, train data 주어질 때 학습된 모델을 반환
+
+
+@dataclass
+class MultivariateNormalDistribution:
+    mean: npt.NDArray[np.float64]
+    cov: npt.NDArray[np.float64]
+    dim: int = 1
+
+    def __post_init__(self) -> None:
+        if self.mean.shape != (self.dim,):
+            raise ValueError(f"Mean shape is {self.mean.shape}, expected {(self.dim,)}")
+        if self.cov.shape != (self.dim, self.dim):
+            raise ValueError(
+                f"Covariance shape is {self.cov.shape},"
+                f"expected {(self.dim, self.dim)}"
+            )
+
+    def pdf(self, x: npt.NDArray[np.float64]) -> np.float64:
+        """Calculate the probability density function of the distribution."""
+        x = x - self.mean
+        return (  # type: ignore
+            np.exp(-0.5 * x @ np.linalg.inv(self.cov) @ x)
+            / np.sqrt((2 * np.pi) ** self.dim * np.linalg.det(self.cov))
+        )
+
+
+MVN: TypeAlias = MultivariateNormalDistribution
