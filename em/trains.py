e05e9fccbab9021d314d74bc31e840a46cd5ca41
Jeongseop Lim <jseoplim@gmail.com>
2024-10-13 03:35:49 +0900

    fix: training

--- em/trains.py
+++ em/trains.py
@@ -23,7 +23,7 @@ if not (train_data_path_env := os.getenv("TRAIN_DATA_PATH")):
     raise ValueError("TRAIN_DATA_PATH environment variable is not set")
 
 train_data_path = Path(train_data_path_env)
-train_dataset = libs.parse_file_to_array(train_data_path)
+train_dataset = libs.parse_file_to_array(train_data_path)[:1000, :]
 
 fold_size = 10
 n_classes = 2
@@ -33,25 +33,43 @@ def generate_init_parameters(
     input_dataset: npt.NDArray[np.float64],
     n_clusters: int,
 ) -> list[libs.GMMParameter]:
+    """EM 알고리즘을 위한 초기 파라미터를 생성합니다.
+
+    Args:
+        input_dataset: (n_samples, n_features) 크기의 2차원 배열
+        n_clusters: 클러스터의 개수
+
+    Returns:
+        n_clusters개의 GMMParameter 객체를 원소로 갖는 리스트
+    """
     return [
         libs.GMMParameter(
             mean=np.mean(input_dataset, axis=0),
-            cov=np.cov(input_dataset, ddof=0, rowvar=False),
+            cov=np.outer(
+                input_dataset - np.mean(input_dataset, axis=0),
+                input_dataset - np.mean(input_dataset, axis=0),
+            )
+            / (input_dataset.shape[0]),
             weight=np.float64(1 / n_clusters),
         )
         for _ in range(n_clusters)
     ]
 
 
-n_clusters_range = range(2, 11)
+n_clusters_range = range(2, 3)
 error_rates_of_n_clusters: list[float] = []
 
+print("Training...")
+
 for n_clusters in n_clusters_range:
+    print(f"  Number of Clusters: {n_clusters}")
+
     error_rates_of_folds: list[float] = []
 
     for fold_k, fold_train_dataset in enumerate(
         libs.generate_dataset_parts(train_dataset, fold_size)
     ):
+        print(f"    Fold: {fold_k + 1}")
 
         def get_input_dataset(class_k: int) -> Any:
             return fold_train_dataset.train[fold_train_dataset.train[:, -1] == class_k]
@@ -66,7 +84,10 @@ for n_clusters in n_clusters_range:
         ]
         classifer = libs.GaussianMixtureModelClassifier(n_classes=n_classes)
         classifer.set_known_parameters(parameters_list=gmm_parameters_list)
-        predicted_outputs = classifer.predict(fold_train_dataset.validation[:, :-1])
+        predicted_outputs = classifer.predict(
+            fold_train_dataset.validation[:, :-1],
+            fold_train_dataset.validation[:, -1],  # type: ignore
+        )
 
         error_rates_of_folds.append(
             libs.calculate_error_rate(
